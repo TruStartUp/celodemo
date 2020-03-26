e037b7010eb99df7f7e716f7822b65e7bfdf09ab
Mesi Rendon <amrendonsa@unal.edu.co>
2020-03-26 17:16:29 -0500

    Basic Accelerometer catch up
            I've made a simple Android app that takes information out of the
            acceleromenter and register a gravity above 13 m/s^2, changing
            the label to red while shaking the mobile device.
    
            Reported by: Mesi Rendon <mesi@trustartup.co>

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,32 +1,92 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page actionBarHidden="true">
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <GridLayout columns="*" rows="*,*">
+      <Label class="message" :text="info" col="0" row="0"/>
+      <Label class="message" :class="{gravity: accReached}" :text="gravity" col="0" row="1"/>
+    </GridLayout>
+  </Page>
 </template>
 
 <script lang="ts">
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    }
+import {
+  Component,
+  Watch,
+  Vue,
+} from 'vue-property-decorator';
+import {
+  AndroidSensors,
+  AndroidSensorListener,
+  SensorDelay,
+} from "nativescript-android-sensors";
+
+@Component({})
+export default class App extends Vue {
+  public msg = 'Hola mundo';
+  public accelerometer = {
+    x_data: null,
+    y_data: null,
+    z_data: null,
+  };
+  public androidSensors: AndroidSensors;
+  public accReached: Boolean = false;
+
+  constructor() {
+    super();
+    this.androidSensors = new AndroidSensors();
+    const mHWListener = new AndroidSensorListener({
+      onSensorChanged: (result) => {
+        const parsedData = JSON.parse(result);
+        const rawSensorData = parsedData.data;
+        this.accelerometer.x_data = rawSensorData.x.toFixed(2);
+        this.accelerometer.y_data = rawSensorData.y.toFixed(2);
+        this.accelerometer.z_data = rawSensorData.z.toFixed(2);
+      },
+      onAccuracyChanged: (sensor, accuracy) => {
+        console.log(`Accuracy changed: Sensor => ${sensor} | Accuracy => ${accuracy}`);
+      },
+    });
+    const acceleration = this.androidSensors.startSensor(
+      android.hardware.Sensor.TYPE_ACCELEROMETER,
+      SensorDelay.NORMAL,
+      8000000,
+    );
+    this.androidSensors.setListener(mHWListener);
   }
+
+  get info(): String {
+    return `X: ${this.accelerometer.x_data} | Y: ${this.accelerometer.y_data} | Z: ${this.accelerometer.z_data}`;
+  }
+
+  @Watch('gravity')
+  onGravityChanged(newVal: Number, oldVal: Number) {
+    this.accReached = newVal >= 13;
+  }
+
+  get gravity(): Number {
+    return parseFloat(Math.sqrt(
+      Math.pow(this.accelerometer.x_data, 2)
+      + Math.pow(this.accelerometer.y_data, 2)
+      + Math.pow(this.accelerometer.z_data, 2)
+    ).toFixed(2));
+  }
+}
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20;
+    color: #333333;
+  }
+
+  .gravity {
+    color: crimson;
+  }
 </style>
--- app/main.ts
+++ app/main.ts
@@ -1,21 +1,27 @@
-import Vue from 'nativescript-vue'
-import App from './components/App'
+import Vue from 'nativescript-vue';
+import App from './components/App.vue';
+import firebase from 'nativescript-plugin-firebase';
 
 import store from './store'
 import VueDevtools from 'nativescript-vue-devtools'
 
-if(TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
 
+firebase.init({})
+  .catch((err) => {
+    console.error(`Firebase error: ${err}`);
+  });
+
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+Vue.config.silent = (TNS_ENV === 'production');
 // Prints Colored logs when --env.production is *NOT* set while building
 // @ts-ignore
-Vue.config.debug = (TNS_ENV !== 'production')
+Vue.config.debug = (TNS_ENV !== 'production');
 
 
 new Vue({
   store,
   render: h => h('frame', [h(App)])
-}).$start()
+}).$start();
